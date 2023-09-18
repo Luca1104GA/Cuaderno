5928725d9ade06e891cfd7f8aa322dcdc4a1944a
Luca <almadaluca1104@gmail.com>
2023-09-18 16:53:29 -0300

    Finalizamos la seccion tablas

--- script.js
+++ script.js
@@ -8,61 +8,58 @@ const subMenuBtn = document.querySelectorAll(".submenu-btn");
 for(let i=0; i<subMenuBtn.length; i++) {
     subMenuBtn[i].addEventListener("click", function(){
         if(window.innerWidth < 1024){
-            const subMenu = this.nextElementSibling
+            const subMenu = this.nextElementSibling;
+            const height = subMenu.scrollHeight;
+
+            if(subMenu.classList.contains("desplegar")){
+                subMenu.classList.remove("desplegar");
+                subMenu.removeAttribute("style")
+            } else {
+                subMenu.classList.add("desplegar");
+                subMenu.style.height = height + "px"
+            }
+
+            
         }
     });
 }
 
 
+const botonVolverArriba = document.getElementById("botonVolverArriba");
+const botonIrAbajo = document.getElementById("botonIrAbajo");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-const botonVolverArriba = document.getElementById('botonVolverArriba');
-const botonIrAbajo = document.getElementById('botonIrAbajo');
-
-botonVolverArriba.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+botonVolverArriba.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-botonIrAbajo.addEventListener('click', () => {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+botonIrAbajo.addEventListener("click", () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
 });
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 200) {
-        botonVolverArriba.style.display = 'block';
+        botonVolverArriba.style.display = "block";
     } else {
-        botonVolverArriba.style.display = 'none';
+        botonVolverArriba.style.display = "none";
     }
 
     if (window.scrollY < document.body.scrollHeight - window.innerHeight) {
-        botonIrAbajo.style.display = 'block';
+        botonIrAbajo.style.display = "block";
     } else {
-        botonIrAbajo.style.display = 'none';
+        botonIrAbajo.style.display = "none";
     }
 });
 
 
-/* document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
         });
     });
-}); */ /* antiguos botones del menu */
+});
 
 const botonTop = document.getElementById("botonTop");
 botonTop.addEventListener("click", () => {
