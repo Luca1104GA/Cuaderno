5f004c05e051b56b5e78cbe2bf3975fe3d5c9e41
Luca <almadaluca1104@gmail.com>
2023-10-24 19:14:50 -0300

    Tengo dos monitores al fin je

--- script.js
+++ script.js
@@ -132,3 +132,16 @@ for (let i = 0; i < botonesChatGpt.length; i++) {
 }
 
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const lista = document.querySelector('.desplegable');
+    const boton = document.querySelector('.button');
+
+    boton.addEventListener('click', function() {
+        if (lista.classList.contains('desplegado')) {
+            lista.classList.remove('desplegado');
+        } else {
+            lista.classList.add('desplegado');
+        }
+    });
+});
